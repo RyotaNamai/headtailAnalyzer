e87ee7dc76405d4c48a3ad0dcf2663c58f15cd8e
RyotaNamai <237s116s@stu.kobe-u.ac.jp>
2024-02-10 23:26:16 +0900

    initial commit

---
+++ source/eventSelector.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+// boost
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+#include "TStyle.h"
+#include "TROOT.h"
+#include "TFile.h"
+#include "TTree.h"
+
+int main(int argc, char** argv) {
+    gStyle->SetOptStat(0);
+    // open root file
+    TFile *file = new TFile("./cvvar.root", "READ");
+    if (!file->IsOpen()) {
+        std::cerr << ">>> Error: rootfile not found." << std::endl;
+        return 1;
+    } else {
+        std::cout << ">>> rootfile opened." << std::endl;
+    }
+    TTree *tree = (TTree*)file->Get("cvvar_tree");
+
+    // read config
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json("../config/cut.json", pt);
+    if (pt.empty()) {
+        std::cerr << ">>> Error: config file not found." << std::endl;
+        return 1;
+    } else {
+        std::cout << ">>> config file opened." << std::endl;
+    }
+    const bool   bool_unixtime = pt.get<bool>("activate.unixtime");
+    const bool   bool_length   = pt.get<bool>("activate.length"  );
+    const bool   bool_x        = pt.get<bool>("activate.x"       );
+    const bool   bool_y        = pt.get<bool>("activate.y"       );
+    const bool   bool_x_ave    = pt.get<bool>("activate.x_ave"   );
+    const bool   bool_y_ave    = pt.get<bool>("activate.y_ave"   );
+    const bool   bool_ene_low  = pt.get<bool>("activate.ene_low" );
+    const bool   bool_ene_high = pt.get<bool>("activate.ene_high");
+    const double unixtime_min = pt.get<double>("unixtime_min");
+    const double unixtime_max = pt.get<double>("unixtime_max");
+    const double length_min   = pt.get<double>("length_min"  );
+    const double length_max   = pt.get<double>("length_max"  );
+    const double x_min        = pt.get<double>("x_min"       );
+    const double x_max        = pt.get<double>("x_max"       );
+    const double y_min        = pt.get<double>("y_min"       );
+    const double y_max        = pt.get<double>("y_max"       );
+    const double x_ave_min    = pt.get<double>("x_ave_min"   );
+    const double x_ave_max    = pt.get<double>("x_ave_max"   );
+    const double y_ave_min    = pt.get<double>("y_ave_min"   );
+    const double y_ave_max    = pt.get<double>("y_ave_max"   );
+    const double ene_low_min  = pt.get<double>("ene_low_min" );
+    const double ene_low_max  = pt.get<double>("ene_low_max" );
+    const double ene_high_min = pt.get<double>("ene_high_min");
+    const double ene_high_max = pt.get<double>("ene_high_max");
+
+    int evtNum;
+    double length;
+    double x_xz_min, x_xz_max;
+    double y_yz_min, y_yz_max;
+    double x_xz_ave, y_yz_ave;
+    double ene_low, ene_high;
+    int unixtime;
+
+    tree->SetBranchAddress("evtNum"  , &evtNum  );
+    tree->SetBranchAddress("length"  , &length  );
+    tree->SetBranchAddress("x_xz_min", &x_xz_min);
+    tree->SetBranchAddress("x_xz_max", &x_xz_max);
+    tree->SetBranchAddress("y_yz_min", &y_yz_min);
+    tree->SetBranchAddress("y_yz_max", &y_yz_max);
+    tree->SetBranchAddress("x_xz_ave", &x_xz_ave);
+    tree->SetBranchAddress("y_yz_ave", &y_yz_ave);
+    tree->SetBranchAddress("ene_low" , &ene_low );
+    tree->SetBranchAddress("ene_high", &ene_high);
+    tree->SetBranchAddress("unixtime", &unixtime);
+
+    const int nEntries = tree->GetEntries();
+
+    // dump selected event number
+    std::ofstream ofs("selectedEvent.dat");
+    std::cout << ">>> electedEvent.dat created." << std::endl;
+    ofs << "# evtNum" << std::endl;
+
+    std::cout << ">>> Start event selection." << std::endl;
+    for (int i = 0; i < nEntries; i++) {
+        tree->GetEntry(i);
+        if (bool_length   == true && (length   < length_min   || length   > length_max  )) continue;
+        if (bool_x        == true && (x_xz_min < x_min        || x_xz_max > x_max       )) continue;
+        if (bool_y        == true && (y_yz_min < y_min        || y_yz_max > y_max       )) continue;
+        if (bool_x_ave    == true && (x_xz_ave < x_ave_min    || x_xz_ave > x_ave_max   )) continue;
+        if (bool_y_ave    == true && (y_yz_ave < y_ave_min    || y_yz_ave > y_ave_max   )) continue;
+        if (bool_ene_low  == true && (ene_low  < ene_low_min  || ene_low  > ene_low_max )) continue;
+        if (bool_ene_high == true && (ene_high < ene_high_min || ene_high > ene_high_max)) continue;
+        if (bool_unixtime == true && (unixtime < unixtime_min || unixtime > unixtime_max)) continue;
+        ofs << evtNum << std::endl;
+        std::cout << "Progress: " << i << " / " << nEntries << "\t[" << Form("%.2f %%]", (double)i / (double)nEntries * 100) << "\r" << std::flush;
+    }
+    std::cout << ">>> Event selection finished." << std::endl;
+    file->Close();
+
+    return 0;
+}
---
+++ source/headtailAnalyzer.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <numeric>
+
+#include "TStyle.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TCanvas.h"
+#include "TPad.h"
+#include "TH2D.h"
+#include "TH1D.h"
+#include "TF1.h"
+#include "TText.h"
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " [evtNum]" << std::endl;
+        return 1;
+    }
+    std::string evtNum = argv[1];
+
+    int opt;
+    int flag_stat = 0;
+
+    while ((opt = getopt(argc, argv, "s")) != -1) {
+        switch (opt) {
+            case 's':
+                flag_stat = 1;
+                break;
+            default:
+                break;
+        }
+    }
+
+    const Int_t NRGBs = 5;
+    const Int_t NCont = 255;
+
+    Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+    Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 1.00 };
+    Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+    Double_t blue[NRGBs]  = { 0.80, 1.00, 0.12, 0.00, 0.00 };
+    TColor::CreateGradientColorTable( NRGBs, stops, red, green, blue, NCont );
+    gStyle->SetNumberContours( NCont );
+
+    gStyle->SetOptStat(0);
+    TFile *file = new TFile("./cvvar.root", "READ");
+    if (!file->IsOpen()) {
+        std::cerr << "Error: rootfile not found." << std::endl;
+        return 1;
+    }
+    TTree *tree = (TTree*)file->Get("cvvar_tree");
+
+    //#########################################//
+    // canvas
+    //#########################################// 
+
+    TCanvas *c1 = new TCanvas("c1", "c1", 800, 400);
+    c1->Draw();
+    c1->Divide(2, 1);
+
+    // x-z
+    c1->cd(1);
+    TPad *pad1 = new TPad("pad1", "pad1", 0.0, 0.0, 1.0, 0.65);
+    pad1->SetTopMargin(0);
+    pad1->SetGrid();
+    pad1->Draw();
+    TPad *pad2 = new TPad("pad2", "pad2", 0.0, 0.65, 1.0, 0.9);
+    pad2->Draw();
+    TPad *pad5 = new TPad("pad5", "pad5", 0.0, 0.9, 1.0, 1.0);
+    pad5->Draw();
+    pad1->cd();
+    TH2D *hist_xz = new TH2D("hist_xz", "", 768, -15., 15., 1000, -41., 0.);
+    tree->Draw("z_xz:x_xz>>hist_xz", Form("evtNum==%s", evtNum.c_str()), "colz");
+    hist_xz->GetXaxis()->SetTitle("x (cm)");
+    hist_xz->GetYaxis()->SetTitle("z (cm)");
+    pad2->cd();
+    TH1D *hist_x = new TH1D("hist_x", "", 768, -15., 15.);
+    hist_x->SetTitleSize(0.2, "Y");
+    hist_x->SetFillColorAlpha(kBlue, 0.1);
+    tree->Draw("x_xz>>hist_x", Form("evtNum==%s", evtNum.c_str()), "bar");
+    pad5->cd();
+    TText *text = new TText(0.5, 0.5, Form("evtNum: %s x-z", evtNum.c_str()));
+    text->SetTextAlign(22);
+    text->SetTextSize(0.5);
+    text->Draw();
+ 
+    // y-z
+    c1->cd(2);
+    TPad *pad3 = new TPad("pad3", "pad3", 0.0, 0.0, 1.0, 0.65);
+    pad3->SetTopMargin(0);
+    pad3->SetGrid();
+    pad3->Draw();
+    TPad *pad4 = new TPad("pad4", "pad4", 0.0, 0.65, 1.0, 0.9);
+    pad4->Draw();
+    TPad *pad6 = new TPad("pad6", "pad6", 0.0, 0.9, 1.0, 1.0);
+    pad6->Draw();
+    pad3->cd();
+    TH2D *hist_yz = new TH2D("hist_yz", "", 1000, -15., 15., 768, -41., 0.);
+    tree->Draw("z_yz:y_yz>>hist_yz", Form("evtNum==%s", evtNum.c_str()), "colz");
+    hist_yz->GetXaxis()->SetTitle("y (cm)");
+    hist_yz->GetYaxis()->SetTitle("z (cm)");
+    pad4->cd();
+    TH1D *hist_y = new TH1D("hist_y", "", 768, -15., 15.);
+    hist_y->SetFillColorAlpha(kBlue, 0.1);
+    tree->Draw("y_yz>>hist_y", Form("evtNum==%s", evtNum.c_str()), "bar");
+    pad6->cd();
+    TText *text2 = new TText(0.5, 0.5, Form("evtNum: %s y-z", evtNum.c_str()));
+    text2->SetTextAlign(22);
+    text2->SetTextSize(0.5);
+    text2->Draw();
+
+    //#########################################//
+    // fit
+    //#########################################//
+    // x-z
+    pad2->cd();
+    TF1 *fit_x = new TF1("fit_x", "pol1", -15., 15.);
+    hist_x->Fit("fit_x", "R");
+    fit_x->SetLineColor(kRed);
+    fit_x->Draw("same");
+    TText *slope_x = new TText(-14, 0, Form("slope: %.2f +- %.2f", fit_x->GetParameter(1), fit_x->GetParError(1)));
+    slope_x->SetTextSize(0.2);
+    slope_x->Draw();
+    // y-z
+    pad4->cd();
+    TF1 *fit_y = new TF1("fit_y", "pol1", -15., 15.);
+    hist_y->Fit("fit_y", "R");
+    fit_y->SetLineColor(kRed);
+    fit_y->Draw("same");
+    TText *slope_y = new TText(-14, 0, Form("slope: %.2f +- %.2f", fit_y->GetParameter(1), fit_y->GetParError(1)));
+    slope_y->SetTextSize(0.2);
+    slope_y->Draw();
+
+    if (flag_stat == 0) {
+        c1->SaveAs(Form("headtail_%s.png", evtNum.c_str()));
+    } else {
+        std::ofstream ofs("./slopeList.dat");
+        ofs << evtNum << "\t" << fit_x->GetParameter(1) << "\t" << fit_x->GetParError(1) << std::endl;
+        ofs.close();
+    }
+
+    file->Close();
+    delete file;
+
+    return 0;
+}
